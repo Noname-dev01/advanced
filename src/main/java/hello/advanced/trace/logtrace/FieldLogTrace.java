ac3c3c336af93afd031ad5b67e07f0403589541f
noname_dev <noname_dev@naver.com>
2022-10-12 17:38:14 +0900

    필드 동기화 - 적용

--- src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
+++ src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
@@ -46,9 +46,11 @@ public class FieldLogTrace implements LogTrace {
         long resultTimeMs = stopTimeMs - status.getStartTimeMs();
         TraceId traceId = status.getTraceId();
         if (e == null){
-            log.info("[{}] {}{} time={}ms", traceId.getId(),addSpace(COMPLETE_PREFIX, traceId.getLevel()));
+            log.info("[{}] {}{} time={}ms", traceId.getId(),addSpace(COMPLETE_PREFIX, traceId.getLevel()),
+                    status.getMessage(),resultTimeMs);
         }else {
-            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(),addSpace(EX_PREFIX, traceId.getLevel()));
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(),addSpace(EX_PREFIX, traceId.getLevel()),
+                    status.getMessage(),resultTimeMs,e.toString());
         }
 
         releaseTraceId();
