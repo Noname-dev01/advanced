0a64d6ea1e51cdb8525134f662ffd580492094b7
noname_dev <noname_dev@naver.com>
2022-10-14 19:02:56 +0900

    쓰레드 로컬 동기화 - 적용

--- src/main/java/hello/advanced/LogTraceConfig.java
+++ src/main/java/hello/advanced/LogTraceConfig.java
@@ -2,15 +2,22 @@ package hello.advanced;
 
 import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogTraceConfig {
 
+//    @Bean
+//    public LogTrace logTrace() {
+//        return new FieldLogTrace();
+//    }
+
+    //쓰레드로컬로 변경
     @Bean
-    public LogTrace logTrace() {
-        return new FieldLogTrace();
+    public LogTrace logTrace(){
+        return new ThreadLocalLogTrace();
     }
 
 }
